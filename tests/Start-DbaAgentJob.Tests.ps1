d43bb6a7df999cac4195da9d939aa5b35f898103
Shawn Melton <wshawnmelton@gmail.com>
2020-01-17 21:39:47 -0600

    (do Start-DbaAgentJob)

--- tests/Start-DbaAgentJob.Tests.ps1
+++ tests/Start-DbaAgentJob.Tests.ps1
@@ -27,4 +27,5 @@ Describe "$CommandName Integration Tests" -Tag "IntegrationTests" {
     It "returns on multiple server inputs" {
         $results = Get-DbaAgentJob -SqlInstance $script:instance2,$script:instance3 -Job 'DatabaseBackup - SYSTEM_DATABASES - FULL' | Start-DbaAgentJob
         ($results.SqlInstance).Count | Should -Be 2
-   }
+    }
+}
